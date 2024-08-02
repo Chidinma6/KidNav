33d46f8768c605532d7b43d561bf690c884e00ed
Chidinma6 <93360464+Chidinma6@users.noreply.github.com>
2024-08-02 11:40:29 +0100

    Update twilio_service.dart

--- lib/sms/twilio_service.dart
+++ lib/sms/twilio_service.dart
@@ -2,9 +2,9 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class TwilioService {
-  final String accountSid = 'ACa772d6efcd34b677ad53aa1f3cfa6e26';
-  final String authToken = 'b503d6b8192785553e50b4fe75298b1b';
-  final String twilioNumber = '+12602362397';
+  final String accountSid = 'Enter your accountsid';
+  final String authToken = 'Enter token';
+  final String twilioNumber = 'enter twilio number';
 
   Future<void> sendSms(String to, String message) async {
     final String url =
